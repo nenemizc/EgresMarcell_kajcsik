cdc8f9be28223e5793dd49fb4ddbda2449fdbe25
Petrik Diak <diak@petrik.hu>
2025-04-04 12:16:00 +0200

    etel

---
+++ kajcsik/Etel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kajcsik
+{
+	internal abstract class Etel
+	{
+		double caloriesPerDkg;
+		double wheight;
+		string name;
+
+		protected Etel(double caloriesPerDkg, double wheight, string name)
+		{
+			this.caloriesPerDkg = caloriesPerDkg;
+			this.wheight = wheight;
+			this.name = name;
+			ItsDairyfree = false;
+		}
+
+		public abstract double CountCalories();
+
+		bool itsDairyfree;
+
+		public bool ItsDairyfree { get => itsDairyfree; set => itsDairyfree = value; }
+	}
+}
